36bd458a71758af47fc363297bfb1e604ebf06b5
Ben Ooms <benoomsdh@gmail.com>
2022-07-12 21:34:37 +0200

    moved frontend to right folder

--- packages/todo-app-frontend/jest.config.ts
+++ packages/todo-app-frontend/jest.config.ts
@@ -1,6 +1,6 @@
 /* eslint-disable */
 export default {
-  displayName: 'todo-app-fronend',
+  displayName: 'todo-app-frontend',
   preset: '../../jest.preset.js',
   setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
   globals: {
@@ -9,7 +9,7 @@ export default {
       stringifyContentPathRegex: '\\.(html|svg)$',
     },
   },
-  coverageDirectory: '../../coverage/packages/todo-app-fronend',
+  coverageDirectory: '../../coverage/packages/todo-app-frontend',
   transform: {
     '^.+\\.(ts|mjs|js|html)$': 'jest-preset-angular',
   },
